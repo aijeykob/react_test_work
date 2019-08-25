d9a8bdac44cf541f61d7c8919cb3d63c6b8c9e6d
AiJey <aijeykob@gmail.com>
2019-08-26 01:57:24 +0300

    Updates

---
+++ precache-manifest.a5257d21117fc06abae876b8c678a1a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24125564f9e0d7841613eaeff4493d32",
+    "url": "/react_test_work/index.html"
+  },
+  {
+    "revision": "6edfd2b2f82f3860d7ac",
+    "url": "/react_test_work/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6edfd2b2f82f3860d7ac",
+    "url": "/react_test_work/static/js/2.3df5e68d.chunk.js"
+  },
+  {
+    "revision": "3c8ae47e1c59bec60a83",
+    "url": "/react_test_work/static/js/main.5b374bcf.chunk.js"
+  },
+  {
+    "revision": "87470993a826e9ed2bb7",
+    "url": "/react_test_work/static/js/runtime~main.f2672d4a.js"
+  }
+]);
